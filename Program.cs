c75f76ff7fa398e3abef22cc4324546a937b4c96
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-09 14:41:40 -0300

    Prácticas consola C sharp para principiantes
    método estructural while y Dowhile

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_y_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool encender = false;
+            do
+            {
+                Console.WriteLine("hola a todos");
+
+            }
+            while (encender);
+            Console.ReadKey();
+        }
+    }
+}
